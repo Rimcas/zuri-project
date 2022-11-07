34d2e582e2f10cab861a1112c0160872732b877d
Rimcas <eguducasidi0@gmail.com>
2022-11-07 04:54:47 -0800

    Add: added the new file where i worked on the footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import zuripro1 from './Components/zuri-pro1'
 import Contact from './Components/contact/contacts'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
